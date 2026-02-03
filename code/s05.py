9bb5ee9d8c422be10bbe569f18dba68af4082cf8
amolendyk1 <amolendyk1@babson.edu>
2026-02-03 16:18:46 -0500

    Add information to READ.ME

--- code/s05.py
+++ code/s05.py
@@ -7,8 +7,15 @@ tax = product * tax_rate
 print(f'The tax for the product which costs ${product} is ${tax}.') # f-string
 
 
-def calc_tax():
-    product = 100 
+computer_price = 900
+iphone_price = 1100
+
+
+def calc_tax(price):
+    """Calculate product tax based on given price"""
     tax_rate = 0.0625
-    tax = product * tax_rate
-    print(f'The tax for the product which costs ${product} is ${tax}.')
+    tax = price * tax_rate
+    print(f'The tax for the product which costs ${price} is ${tax}.')
+
+calc_tax(computer_price)
+calc_tax(iphone_price)
