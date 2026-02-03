8719c40e7fab8cccbe4b92e11edb0ccb528a77be
amolendyk1 <amolendyk1@babson.edu>
2026-02-03 10:45:36 -0500

    Add information to READ.ME

--- code/s05.py
+++ code/s05.py
@@ -9,3 +9,8 @@ if a % 2 == 0:
     print("Even")
 else:
     print("Odd")
+
+a = 2
+1 + a # an expression, which evaluates to 3, but does not do anything with it
+
+print(1 + a) # a statement, which prints the result
