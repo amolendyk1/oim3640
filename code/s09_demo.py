c6899df264332d7e4bf3f556575a0454c7e4ec1f
amolendyk1 <amolendyk1@babson.edu>
2026-02-19 10:43:34 -0500

    Add information to READ.ME

---
+++ code/s09_demo.py
@@ -0,0 +1,20 @@
+for i in range(5):
+    print(i)
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+response = ""
+while response != "quit":
+    response = input("hi: ")
+    print(f"You said: {quit}")
+
+
+# Example 1: Simple counter
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+# Output: 0, 1, 2, 3, 4
