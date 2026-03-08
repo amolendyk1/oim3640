f0554e14e7b5446f09a9605bd6ff83c9b0773281
amolendyk1 <amolendyk1@babson.edu>
2026-03-08 16:57:30 -0400

    Add information to READ.ME

--- code/spelling_beesolver.py
+++ code/spelling_beesolver.py
@@ -5,24 +5,32 @@ def uses_only(word, letters):
             return False
     return True
 
-def uses_all(word, letters):
+def uses_necessary(word, required):
     """Does word use all necessary letters?"""
-    for letter in words:
-        if letter not in letters:
-            return False
-    return True
-
-def uses_all(word, letters):
-    """Does word use long enough letters?"""
     for letter in word:
-        if letter not in letters:
-            return False
-    return True
+        if letter == required:
+            return True
+    return False
+
+def accepted_words(word, letters, required):
+    """Is it an accepted word?"""
+    return uses_only(word, letters) and uses_necessary(word, required) and len(word) >= 4
 
 def find_words(letters, required):
     """Finds words that use only the allowed letters and include the center letter."""
-    with open("C:\\Users\\amolendyk1\\Desktop\\oim3640\\data\\words.txt", "r") as word_file:
+    valid_words = []
+    with open("C:\\Users\\amolendyk1\\Desktop\\oim3640\\data\\words.txt") as word_file:
         for word in word_file:
             word = word.strip()
-            if uses_only(word, letters) and uses_all(word, required):
-                print(word)
+            if accepted_words(word, letters, required):
+                valid_words.append(word)
+    return valid_words
+
+def total(letters, required):
+    words = find_words(letters, required)
+    print(words)
+
+if __name__ == "__main__":
+    letters = "kcboela"
+    required = "a"
+    total(letters, required)
